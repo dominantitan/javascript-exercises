3cba1888fc7f488deef046cc0c9428f40f4f6a35
Zeroth <kingaadit3@gmail.com>
2025-12-18 15:10:47 +0545

    finish 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringSize = string.length;
+    let reversedWord = "";
+    for ( let i = stringSize-1; i >= 0; i--){
+        reversedWord += string[i];
+    }
+    return reversedWord;
 };
 
 // Do not edit below this line
