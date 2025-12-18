6a193befdd59d0a4bcb8f9da418b3c2e20b76a5f
Zeroth <kingaadit3@gmail.com>
2025-12-18 14:43:40 +0545

    finish 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    word = "";
+    if(num < 0){
+        return "ERROR";
+    }
+    for(i=0;i<num;i++){
+        word += string;
+    }
+    return word;
 };
 
 // Do not edit below this line
