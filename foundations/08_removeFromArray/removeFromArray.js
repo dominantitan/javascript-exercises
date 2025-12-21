151cac7deb37dc23792af76690af3823c721b746
Zeroth <kingaadit3@gmail.com>
2025-12-21 17:44:21 +0545

    Finish 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...args) {
+    let elementToRemove = [];
+    for(let i = 0; i < args.length;i++){
+        elementToRemove[i] = args.at(i);
+    }
+
+    for(let i = 0; i < array.length;i++){
+        for(let j = 0;j < elementToRemove.length;j++){
+            if(array[i] === elementToRemove[j]){
+                array.splice(i,1);
+                i -= 1;
+            }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
