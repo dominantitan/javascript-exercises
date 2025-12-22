d00ca89cb925c74e47a759ad65ff9213801b5f71
Zeroth <kingaadit3@gmail.com>
2025-12-22 17:26:01 +0545

    Finish tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(TempInFahrenheit) {
+    let TempInCelsius;
+
+    TempInCelsius = (TempInFahrenheit - 32) * (5/9);
+    let C = round(TempInCelsius,1);
+    return C;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(TempInCelsius) {
+    let TempInFahrenheit;
+
+    TempInFahrenheit = (TempInCelsius*(9/5)+32);
+    let F = round(TempInFahrenheit,1);
+    return F;
 };
+
+function round(number,precision){
+    multiplier = Math.pow(10,precision || 0);
+    return Math.round(number * multiplier)/multiplier;
+}
 
 // Do not edit below this line
 module.exports = {
