9b0f6ce2101c9366bd3f4016ea2bbd54d18d6836
Zeroth <kingaadit3@gmail.com>
2025-12-22 22:29:12 +0545

    Finished 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+  return (num1+num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return (num1-num2);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let s = 0;
+  for(let i = 0;i < array.length;i++){
+    s += array[i];
+  }
+  return s;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let product = 1;
+  for(let i = 0;i<array.length;i++){
+    product *= array[i];
+  }
 
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+	return (Math.pow(num1,num2));
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+  let fact = 1;
+	for(let i = 0;i<num1;i++){
+    fact *= (num1-i);
+  }
+
+  return fact;
 };
 
 // Do not edit below this line
