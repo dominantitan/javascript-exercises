698545dff49f991d903f44c1f9fee7d50adf854f
Zeroth <kingaadit3@gmail.com>
2025-12-24 00:00:11 +0545

    Finish 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let backwardString = "";
+    let noSpaceString = omitSpacesNshit(string);
 
+    for(let i = noSpaceString.length - 1;i >= 0;i--){
+        backwardString += noSpaceString.at(i);
+    }
+    
+    let frontString = noSpaceString.toLowerCase();
+    let backString = backwardString.toLowerCase();
+
+    if(frontString == backString){
+        return true;
+    }else{
+        return false;
+    }
 };
+
+function omitSpacesNshit(string){
+    let s = string.replace(/[ ,!.']/g,"");
+    return s;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
