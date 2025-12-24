a264b1d94e9791fa33e784095f467cdd128bc212
Zeroth <kingaadit3@gmail.com>
2025-12-24 23:40:35 +0545

    Finished 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let a = 1;
+    let b = 1;
+    let number = Number(num);
+    let temp;
+    let arr = [a,b];
+
+    if (number < 0){
+        return "OOPS";
+    }if (number == 0){
+        return 0;
+    }else{
+
+    for(let i = 3;i <= number;i++){
+        temp = a + b;
+        a = b;
+        b = temp;
+        arr.push(temp);
+    }
+
+    return arr[number-1];
+    }
 
 };
 
