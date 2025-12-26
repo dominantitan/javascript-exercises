ab3f29457875663697b7332688e98cc08aa7e2ed
Zeroth <kingaadit3@gmail.com>
2025-12-26 19:15:38 +0545

    15_getTheTitles

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+    let titleArray = [];
 
+    for(let i = 0;i < array.length;i++){
+        titleArray[i] = array[i].title;
+    }
+
+    return titleArray;
 };
 
 // Do not edit below this line
