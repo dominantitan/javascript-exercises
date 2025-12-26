85311a718109c86997c68f6f757970c7b7a1fd82
Zeroth <kingaadit3@gmail.com>
2025-12-26 20:26:12 +0545

    Finish 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let age;
+    let OldestAge = 0;
+    let Oldest;
 
+    array.forEach(element => {
+        if(!(element.yearOfDeath)){
+            age = 2025 - element.yearOfBirth
+        }else{
+            age = element.yearOfDeath - element.yearOfBirth;
+        }
+        if(age > OldestAge){
+            OldestAge = age;
+            Oldest = element;
+        }
+    });
+
+    return Oldest;
 };
 
 // Do not edit below this line
