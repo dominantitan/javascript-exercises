e9fa661fbad8f67f946cfebeec551672c911eeb8
dominantitan <kingaadit3@gmail.com>
2026-01-09 09:49:05 +0545

    Fix and clean sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,21 +1,19 @@
 const sumAll = function(num1,num2) {
-    if(Number.isInteger(num1) && Number.isInteger(num2) && num1 > 0 && num2 > 0){
+    if(!(Number.isInteger(num1) && Number.isInteger(num2) && num1 > 0 && num2 > 0)){
+        return "ERROR"
+    }
 
     let greaterInteger = (num1 > num2)?num1:num2;
     let smallerInteger = (num1 == greaterInteger)?num2:num1
-    let sum = 0;
-    let temp;
+    let array = [];
 
-    for(let i = 0;temp != greaterInteger;i++){
-        temp = smallerInteger + i;
-        sum += temp;
+    for(let i = smallerInteger;i <= greaterInteger;i++){
+        array.push(i);
     }
 
+    let sum = array.reduce((sum,current) => sum + current,0);
     return sum;
 
-    }else {
-        return "ERROR";
-    }
 };
 
 // Do not edit below this line
